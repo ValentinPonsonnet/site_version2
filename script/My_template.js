c3e801c5bb80bf445d306086a8a2e747035dbe8f
Valentin <Ponsonnetvalentin@gmail.com>
2021-12-15 15:23:14 +0100

    initial commit

---
+++ script/My_template.js
@@ -0,0 +1,4 @@
+function template(parent,type,modele)
+{
+    
+}
---
+++ script/script.js
@@ -0,0 +1,24 @@
+fetch("http://ergast.com/api/f1/current/last/results.json")
+    .then((reponse) => {
+        return reponse.json();
+    })
+    .then((data) => {
+       
+        function show_last_winner()
+        {
+            let modele =`
+            <h2>Le dernier gagnant :</h2>
+            <p>Nom du circuit : ${data.MRData.RaceTable.Races[0].Circuit.circuitName}</p>
+            <p>Nom et prénom du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Driver.givenName} ${data.MRData.RaceTable.Races[0].Results[0].Driver.familyName}</p>
+            <p>Ecurie du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Constructor.name}</p>
+            <p>Numéro du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Driver.permanentNumber}</p>
+            <p>Code d'identification : ${data.MRData.RaceTable.Races[0].Results[0].Driver.code}</p>
+            <p>Status de la course : ${data.MRData.RaceTable.Races[0].Results[0].status} </p>`;
+
+            let element = document.querySelector('#last_winner')
+            element.innerHTML = modele;  
+        };
+        show_last_winner()
+    })
+
+    
