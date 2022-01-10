7801c59f64f33e2b80e76f8206a69a7c079cb872
Valentin <Ponsonnetvalentin@gmail.com>
2022-01-10 17:34:36 +0100

    Ajout de l'api météo pour connaitre la météo actuel

--- script/function.js
+++ script/function.js
@@ -5,14 +5,13 @@ export function showLastWinner()
             return reponse.json();
         })
         .then(data => {
-        
                 let modele =`
                 <h2>Le dernier gagnant :</h2>
                 <p>Année du championnat : ${data.MRData.RaceTable.season}</p>
                 <p>Manche du championnat : ${data.MRData.RaceTable.Races[0].round}</p>
                 <p>Nom du circuit : ${data.MRData.RaceTable.Races[0].Circuit.circuitName}</p>
                 <p>Localisation GPS du circuit : ${data.MRData.RaceTable.Races[0].Circuit.Location.lat}, ${data.MRData.RaceTable.Races[0].Circuit.Location.long}
-                <p>Nom et prénom du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Driver.familyName} ${data.MRData.RaceTable.Races[0].Results[0].Driver.givenName} </p>
+                <p>Nom et prénom du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Driver.familyName} ${data.MRData.RaceTable.Races[0].Results[0].Driver.givenName}</p>
                 <p>Nationnalité du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Driver.nationality}</p> 
                 <p>Ecurie du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Constructor.name}</p>
                 <p>Numéro permanent du pilote : ${data.MRData.RaceTable.Races[0].Results[0].Driver.permanentNumber}</p>
@@ -20,10 +19,30 @@ export function showLastWinner()
                 <p>Status de la course : ${data.MRData.RaceTable.Races[0].Results[0].status} </p>`;
 
                 let element = document.querySelector('#last_winner')
-                element.innerHTML = modele;  
+                element.innerHTML = modele; 
+                return data; 
         })
+        .then(data =>{
+            const api_keys = "c836698c33672d5da0b0489e218826f6"
+            const latitude = data.MRData.RaceTable.Races[0].Circuit.Location.lat
+            const longitude = data.MRData.RaceTable.Races[0].Circuit.Location.long
+            let geo = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_keys}&units=metric&lang=fr`;
         
-    fetch("")
+            fetch(geo)
+            .then(response2 => {
+                return response2.json();
+                    })
+                        .then(data2 =>{
+                        let u = "http://openweathermap.org/img/wn/"+data2.weather[0].icon+".png"
+                        let modèle2 =`
+                        <h2>La météo actuel au dessus du circuit :</h2>
+                        <p>Température : ${data2.main.temp}°C</p>
+                        <p><img src="${u}"</p>
+                        `
+                        let element2 = document.querySelector(`#track_weather`)
+                        element2.innerHTML = modèle2
+                        })
+        })
+                            
 }
-
     
